090abd9bc33c76675b77838091f40b249c011ec2
Pawel <kotoniak@gmail.com>
2019-04-26 00:11:32 +0200

    Updates

---
+++ precache-manifest.1369ad8e487395a48efe011e1d4a90c8.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c4eb6c8cc90f9ac50c46d104c2c1169",
+    "url": "/krakowiak/static/media/salabankietowa.2c4eb6c8.png"
+  },
+  {
+    "revision": "5c6764dba065c778233a",
+    "url": "/krakowiak/static/css/main.b7c30c2d.chunk.css"
+  },
+  {
+    "revision": "c2b62eb834886467897f",
+    "url": "/krakowiak/static/js/runtime~main.c93c6460.js"
+  },
+  {
+    "revision": "922cb3bc86ec0568c82525e315387021",
+    "url": "/krakowiak/static/media/Poppins-Bold.922cb3bc.ttf"
+  },
+  {
+    "revision": "c494ce60358668159e19",
+    "url": "/krakowiak/static/js/2.4306dd17.chunk.js"
+  },
+  {
+    "revision": "c28af3c735a157a610cf1549a97ae225",
+    "url": "/krakowiak/static/media/rooms-header.c28af3c7.png"
+  },
+  {
+    "revision": "5b295d7400a27bf84ee3eaacf697005c",
+    "url": "/krakowiak/static/media/footer-logo.5b295d74.png"
+  },
+  {
+    "revision": "2e485fcd09021d32872a391b29aef622",
+    "url": "/krakowiak/static/media/slider.2e485fcd.png"
+  },
+  {
+    "revision": "31cf1a4a360033c49a3e3175adc64a94",
+    "url": "/krakowiak/static/media/struktura-pokoi.31cf1a4a.png"
+  },
+  {
+    "revision": "67ba82c2232ee8d2c828594708e033c1",
+    "url": "/krakowiak/static/media/pokoj1.67ba82c2.png"
+  },
+  {
+    "revision": "49f125d95f9ab077cc2c76527642f184",
+    "url": "/krakowiak/static/media/pokoj1.49f125d9.png"
+  },
+  {
+    "revision": "94af18e08519a5ebfacd96fac64b34ab",
+    "url": "/krakowiak/static/media/pokoj2.94af18e0.png"
+  },
+  {
+    "revision": "a9368f6ca2daa67be8a9fb36b3ff9c23",
+    "url": "/krakowiak/static/media/pokoj2.a9368f6c.png"
+  },
+  {
+    "revision": "c22f4f76c7dd8ea74228d97dc12d8690",
+    "url": "/krakowiak/static/media/pokoj3.c22f4f76.png"
+  },
+  {
+    "revision": "f07cd47fd39c2ea6e7e8e2b8a01f960b",
+    "url": "/krakowiak/static/media/pokoj3.f07cd47f.png"
+  },
+  {
+    "revision": "961c582f824632337867238038fe7df8",
+    "url": "/krakowiak/static/media/pokoj4.961c582f.png"
+  },
+  {
+    "revision": "c54b85e6ec609523deecbe175eca4aa3",
+    "url": "/krakowiak/static/media/pokoj4.c54b85e6.png"
+  },
+  {
+    "revision": "9c4d5d878de6a0088226e135acfa6939",
+    "url": "/krakowiak/static/media/pokoj5.9c4d5d87.png"
+  },
+  {
+    "revision": "7682c3a6a86a650d155da57b4f138f01",
+    "url": "/krakowiak/static/media/pokoj5.7682c3a6.png"
+  },
+  {
+    "revision": "5c6764dba065c778233a",
+    "url": "/krakowiak/static/js/main.dbf87d08.chunk.js"
+  },
+  {
+    "revision": "a3bef338e653ff7c38d936e336e9b3ed",
+    "url": "/krakowiak/static/media/urodzinydladzieci.a3bef338.png"
+  },
+  {
+    "revision": "6647bff02d4616550037c60d4778182a",
+    "url": "/krakowiak/static/media/contact-bg2.6647bff0.png"
+  },
+  {
+    "revision": "49f125d95f9ab077cc2c76527642f184",
+    "url": "/krakowiak/static/media/1.49f125d9.png"
+  },
+  {
+    "revision": "a9368f6ca2daa67be8a9fb36b3ff9c23",
+    "url": "/krakowiak/static/media/2.a9368f6c.png"
+  },
+  {
+    "revision": "f07cd47fd39c2ea6e7e8e2b8a01f960b",
+    "url": "/krakowiak/static/media/3.f07cd47f.png"
+  },
+  {
+    "revision": "c54b85e6ec609523deecbe175eca4aa3",
+    "url": "/krakowiak/static/media/4.c54b85e6.png"
+  },
+  {
+    "revision": "69836d57d42a8738d6bbe1bb157e695b",
+    "url": "/krakowiak/static/media/header.69836d57.png"
+  },
+  {
+    "revision": "d4704952681197eddd6958489754c768",
+    "url": "/krakowiak/static/media/1.d4704952.png"
+  },
+  {
+    "revision": "d5f7e367190109812aa1e979012e2e15",
+    "url": "/krakowiak/static/media/contact-header.d5f7e367.png"
+  },
+  {
+    "revision": "a3b52b397fa04fa22fa662d715b5986d",
+    "url": "/krakowiak/static/media/offer-header.a3b52b39.png"
+  },
+  {
+    "revision": "e9ed4e8ab55dbe5ac078cdb27153836a",
+    "url": "/krakowiak/static/media/struktura-pokoi.e9ed4e8a.png"
+  },
+  {
+    "revision": "2534cb5f01a9c066a986afe1a05031b3",
+    "url": "/krakowiak/static/media/1.2534cb5f.png"
+  },
+  {
+    "revision": "2a71d856a6603d9feb12ff99c667ea97",
+    "url": "/krakowiak/static/media/logo.2a71d856.png"
+  },
+  {
+    "revision": "fcac2dd0845b381fd34f0d902dd6e87b",
+    "url": "/krakowiak/static/media/Poppins-Light.fcac2dd0.ttf"
+  },
+  {
+    "revision": "dd1aed50244d7243b9209dc901c8d1af",
+    "url": "/krakowiak/static/media/Poppins-Regular.dd1aed50.ttf"
+  },
+  {
+    "revision": "5bb3128376ff7ca534d894e470f1c2e9",
+    "url": "/krakowiak/static/media/Poppins-SemiBold.5bb31283.ttf"
+  },
+  {
+    "revision": "28bc024aa3164e0b0ff2d709f17b333f",
+    "url": "/krakowiak/static/media/Poppins-Medium.28bc024a.ttf"
+  },
+  {
+    "revision": "c494ce60358668159e19",
+    "url": "/krakowiak/static/css/2.42af4b9c.chunk.css"
+  },
+  {
+    "revision": "b5385b29c6bbf5721b19896732957eff",
+    "url": "/krakowiak/index.html"
+  }
+];
